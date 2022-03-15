bb113a6dce298224a080e985101c860cf7993e77
unknown <amdapac39@iiht.tech>
2022-03-15 17:07:24 +0800

    first commit

---
+++ difference.java
@@ -0,0 +1,22 @@
+package assignments;
+import java.util.Scanner;
+
+public class {
+	static int x, y, res, res1;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter first number: ");
+		x = sc.nextInt();
+		
+		System.out.println("Enter second number: ");
+		y = sc.nextInt();
+
+		System.out.println("Subtraction result: ");
+		res1 = x + y;
+		System.out.println(res1);
+
+
+	}
+}
